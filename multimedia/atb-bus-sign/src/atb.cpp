c550dee482ba562d4183044858343db2b7ef9d2f
Sven Schwermer <sven@svenschwermer.de>
2018-09-06 12:01:04 +0200

    Working version with OpenWrt build system

--- multimedia/atb-bus-sign/src/atb.cpp
+++ multimedia/atb-bus-sign/src/atb.cpp
@@ -31,16 +31,70 @@ struct departure
     bool is_realtime_data;
 };
 
-static http::dynamic_body request_departures(std::string node_id);
 static std::vector<departure> parse_departures(std::istream &resp);
 static boost::posix_time::ptime parse_timestamp(std::string s);
+
+template<typename ResponseBody>
+static void request_departures(std::string node_id, http::response<ResponseBody> &res)
+{
+    auto const target = api_path + node_id;
+
+    // The io_context is required for all I/O
+    boost::asio::io_context ioc;
+
+    // The SSL context is required, and holds certificates
+    namespace ssl = boost::asio::ssl;
+    ssl::context ctx{ssl::context::sslv23_client};
+
+    // These objects perform our I/O
+    using tcp = boost::asio::ip::tcp;
+    tcp::resolver resolver{ioc};
+    ssl::stream<tcp::socket> stream{ioc, ctx};
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str()))
+    {
+        boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+        throw boost::system::system_error{ec};
+    }
+
+    // Look up the domain name
+    auto const results = resolver.resolve(host, port);
+
+    // Make the connection on the IP address we get from a lookup
+    boost::asio::connect(stream.next_layer(), results.begin(), results.end());
+
+    // Perform the SSL handshake
+    stream.handshake(ssl::stream_base::client);
+
+    // Set up an HTTP GET request message
+    http::request<http::string_body> req{http::verb::get, target, 11};
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    // Send the HTTP request to the remote host
+    http::write(stream, req);
+
+    // This buffer is used for reading and must be persisted
+    boost::beast::flat_buffer buffer;
+
+    // Receive the HTTP response
+    http::read(stream, buffer, res);
+
+    if (res.result_int() != 200)
+        throw std::runtime_error("Error returned by server");
+
+    // Gracefully close the stream
+    boost::system::error_code ec;
+    stream.shutdown(ec);
+}
 
 std::string get_bus_sign(std::string node_id, std::initializer_list<std::string> lines,
                          std::chrono::milliseconds pre_wait)
 {
-    auto response = request_departures(node_id);
-    std::stringstream ss;
-    ss << boost::beast::buffers(response);
+    http::response<http::string_body> response;
+    request_departures(node_id, response);
+    std::stringstream ss(response.body());
     auto departures = parse_departures(ss);
 
     auto now = boost::posix_time::second_clock::local_time();
@@ -69,65 +123,6 @@ std::string get_bus_sign(std::string node_id, std::initializer_list<std::string>
             result += '?';
     }
     return result;
-}
-
-static http::dynamic_body request_departures(std::string node_id)
-{
-    auto const target = api_path + node_id;
-
-    // The io_context is required for all I/O
-    boost::asio::io_context ioc;
-
-    // The SSL context is required, and holds certificates
-    namespace ssl = boost::asio::ssl;
-    ssl::context ctx{ssl::context::sslv23_client};
-
-    // These objects perform our I/O
-    using tcp = boost::asio::ip::tcp;
-    tcp::resolver resolver{ioc};
-    ssl::stream<tcp::socket> stream{ioc, ctx};
-
-    // Set SNI Hostname (many hosts need this to handshake successfully)
-    if (!SSL_set_tlsext_host_name(stream.native_handle(), host))
-    {
-        boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
-        throw boost::system::system_error{ec};
-    }
-
-    // Look up the domain name
-    auto const results = resolver.resolve(host, port);
-
-    // Make the connection on the IP address we get from a lookup
-    boost::asio::connect(stream.next_layer(), results.begin(), results.end());
-
-    // Perform the SSL handshake
-    stream.handshake(ssl::stream_base::client);
-
-    // Set up an HTTP GET request message
-    http::request<http::string_body> req{http::verb::get, target, 11};
-    req.set(http::field::host, host);
-    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-    // Send the HTTP request to the remote host
-    http::write(stream, req);
-
-    // This buffer is used for reading and must be persisted
-    boost::beast::flat_buffer buffer;
-
-    // Declare a container to hold the response
-    http::response<http::dynamic_body> res;
-
-    // Receive the HTTP response
-    http::read(stream, buffer, res);
-
-    if (res.result_int() != 200)
-        throw std::runtime_error("Error returned by server");
-
-    // Gracefully close the stream
-    boost::system::error_code ec;
-    stream.shutdown(ec);
-
-    return res.body();
 }
 
 static std::vector<departure> parse_departures(std::istream &resp)
